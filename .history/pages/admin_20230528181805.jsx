b146c46eb5475d34f75c0d83e80a3bf0488883a0
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-05-28 18:38:23 +0530

    Change repo to front end only and some minor additions

---
+++ .history/pages/admin_20230528181805.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+function SignIn() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Authenticate the user using your backend API
+    
+  };
+
+  return (
+    <div>
+      <h1>Admin Sign In</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignIn;
